45aba7d6f6db5e638cdea9608f4eef8cd8b20aba
Yohann Melo <yohann.melo@live.fr>
2018-05-12 09:33:40 -0400

    Flavored libraries, with features accessible only to the full library version

---
+++ testlib/src/main/java/com/ymelo/testlib/Configuration.java
@@ -0,0 +1,13 @@
+package com.ymelo.testlib;
+
+/**
+ * Created by Ymelo on 5/6/2018.
+ */
+
+public class Configuration {
+    private int anInt = 0;
+
+    public int getInt() {
+        return anInt;
+    }
+}
---
+++ testlib/src/restricted/java/com/ymelo/testlib/SuperConfiguration.java
@@ -0,0 +1,9 @@
+package com.ymelo.testlib;
+
+/**
+ * Created by Ymelo on 5/6/2018.
+ */
+
+public class SuperConfiguration {
+    public int otherInt = 2;
+}
